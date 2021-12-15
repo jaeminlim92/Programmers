024e430f7ffbbe024013b3b008bb899cb19cf0a5
jaeminlim92 <jaminlim159@gmail.com>
2021-12-15 22:18:11 +0900

    테스트 커밋 

--- src/lottomintomax/LottoMinToMax.java
+++ src/lottomintomax/LottoMinToMax.java
@@ -27,7 +27,7 @@ public class LottoMinToMax {
         return  answer;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         LottoMinToMax lotto_max_min = new LottoMinToMax();
 
 
