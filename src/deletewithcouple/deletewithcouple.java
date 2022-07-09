c3d709f9eac8a6d49e36c3b747d0243abf97d305
jaeminlim <jaminlim159@gmail.com>
2022-07-09 17:16:04 +0900

    Lv2. 짝지어 제거하기 문제 - Stack을 이용하여 문제 풀이

---
+++ src/deletewithcouple/deletewithcouple.java
@@ -0,0 +1,43 @@
+package deletewithcouple;
+
+import java.util.Stack;
+
+/**
+ * 프로그래머스 문제 짝지어 제거하기
+ * packageName    : deletewithcouple
+ * fileName       : deletewithcouple
+ * author         : jaeminlim
+ * date           : 2022/07/09
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/07/09        jaeminlim       최초 생성
+ */
+public class deletewithcouple {
+
+    public int deleteIfCouple(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char word = s.charAt(i);
+
+            if(!stack.isEmpty() && stack.peek() == word){
+                stack.pop();
+            }else {
+                stack.push(word);
+            }
+        }
+
+        if(stack.isEmpty()) return 1;
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
