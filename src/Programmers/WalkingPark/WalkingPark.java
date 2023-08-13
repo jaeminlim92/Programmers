07ebf9f82aba91019e36668d5be9b4f65a317e76
jaeminlim <jaminlim159@gmail.com>
2023-08-13 22:53:48 +0900

    Feat: 프로그래머스 공원산책 정답반영

--- src/Programmers/WalkingPark/WalkingPark.java
+++ src/Programmers/WalkingPark/WalkingPark.java
@@ -6,8 +6,8 @@ public class WalkingPark {
 
     public static void main(String[] args) {
 
-        String[] park = {"SOO","OXX","OOO"};
-        String[] routes = {"E 2","S 2","W 1"};
+        String[] park = {"OSO","OOO","OXO","OOO"};
+        String[] routes = {"E 2","S 3","W 1"};
 
         int[] solution = solution(park, routes);
         System.out.println("solution = " + Arrays.toString(solution));
@@ -32,13 +32,45 @@ public class WalkingPark {
             }
         }
 
-
-
-        System.out.println("parkArray = " + Arrays.deepToString(parkArray));
-        System.out.println("robot_x = " + robot_x);
-        System.out.println("robot_y = " + robot_y);
-
         // 3.routes를 split하여 각 변수에 담는다.
+        for (int i = 0; i < routes.length; i++) {
+            String direction = routes[i].split(" ")[0];
+            int move = Integer.parseInt(routes[i].split(" ")[1]);
+            int dx = robot_x;
+            int dy = robot_y;
+
+
+            //move에 따라 한칸씩 옮길 때마다 해당 유효성 체크를 시작한다.
+            for (int j = 0; j < move; j++) {
+
+                if(direction.equals("E")){
+                    dy++;
+                }
+                if(direction.equals("W")){
+                    dy--;
+                }
+                if(direction.equals("N")){
+                    dx--;
+                }
+                if(direction.equals("S")){
+                    dx++;
+                }
+
+
+                if (0 <= dx && dx < parkArray.length && 0 <= dy && dy < parkArray[0].length) {
+                    if (parkArray[dx][dy] == 'X') {
+                        break;
+                    }
+
+                    if (j == move - 1) {
+                        robot_x = dx;
+                        robot_y = dy;
+                    }
+
+                }
+            }
+        }
+
         // 4.유효성 검증을 체크한다.(장애물 체크, 공원 범위 체크)
         // 5.이상이 없을 경우 해당 이동수 만큼 이동한다.
 
